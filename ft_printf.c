f1989f65a0fbca75df7c43014a09360e715df9ea
Denis Lom <lom.denis@seznam.cz>
2023-02-12 19:17:00 +0100

    ft_printf.c notes from books added

--- ft_printf.c
+++ ft_printf.c
@@ -26,6 +26,27 @@ FROM: PRINZ, Peter a Ulla KIRCH-PRINZ. C Pocket Reference. O'REILLY, 2003. ISBN 
 4. After reading out the argument list the function should invoke the va_end macro before returning
 	control to the caller.
 	- The only parametre of va_end is the arglist object.
+
+FROM: PRINZ, Peter a Tony. C in a Nutshell: THE DEFINITIVE REFERENCE.
+2nd Edition. Sebastopol: O'REILLY, 2016. ISBN 978-1-491-90475-6.
+EXAMPLE OF PRINTF
+int	score = 120;
+char	player[] = "Mary";
+printf("%s has %d points. \n", player, score);
+
+THE HEADER stdarg.h defines one type and four macros for use in accessing the optional
+arguments of functions that support them.
+va_list: object commonly called an argument pointer, serves as a reference to a list
+	of optional arguments.
+va_start: macro, sets the argument pointer to the first optional argument in the list.
+va_arg(): macro, returns the current argument and sets the argument pointer to the
+	next one in the list.
+va_copy(): macro, copies the va_list object in its current state.
+va_end(): macro, cleans up after the use of a va+list object. A function with a variable
+	number of arguments must contain a va_end() macro call corresponding to each invocation
+	of va_start() or va_copy().
+
+Macros va_copy() and va_end() may also be implemented as functions.
 */
 int	ft_printf(char *str, ...)
 {
