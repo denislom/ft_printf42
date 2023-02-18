b675a1b1ef046a00353e832342453c704c8b1491
Denis Lom <lom.denis@seznam.cz>
2023-02-18 22:48:31 +0100

    ft_print_pointer.c and ft_putstr.c changed

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,12 +6,19 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 17:15:09 by dlom              #+#    #+#             */
-/*   Updated: 2023/02/18 17:19:36 by dlom             ###   ########.fr       */
+/*   Updated: 2023/02/18 20:31:24 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+/*
+uintptr_t is an unsigned integer type that is capable of holding a pointer
+value, which means it can store the memory address of an object in the 
+system's address space.
+Necessary to include # include <stdint.h>
+
+*/
 int	ft_print_pointer(uintptr_t num)
 {
 	
